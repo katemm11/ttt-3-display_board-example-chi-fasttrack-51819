20833b87fe9de84b9c10058a95264d3c5d2a7475
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-18 17:04:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,8 @@
 def display_board
   puts "Welcome to Tic Tac Toe"
   puts "   |   |   "
-  puts "\n-----------\n"
+  puts "-----------"
   puts "   |   |   "
-  puts "\n-----------\n"
+  puts "-----------"
   puts "   |   |   "
 end
